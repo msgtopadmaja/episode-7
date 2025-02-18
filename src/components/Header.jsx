1afaa45fb038f53efb105e460711563a9b96d7a9
rslpadmaja@gmail.com <Padmaja>
2025-02-18 08:26:10 +0530

    completed upto the child routing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router";
 
 const Header = () => {
   let btnName = "Login"; // the normal JS variable cannot able to track the value changes by react, so we are using the useState variable
   const [btnNameReact, setBtnNameReact] = useState("Login");
   console.log("header rendered"); // whole header component render when the state variable changes and see the diff b/w two Vitural DOM and update the those changes only
+
+  // if no dependencies array => useEffect is called on every render
+  // if dependencies array is empty [] => useEffect is called on initial render(just once)
+  // if dependencies array [btnNameReact]=> called everytime btnNameReact is updated
+
+  useEffect(() => {
+    console.log("useEffect() called");
+  }, [btnNameReact]);
 
   return (
     <div className="header">
@@ -13,9 +22,15 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About us</li>
-          <li>Contact us</li>
+          <li>
+            <Link>Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact us</Link>
+          </li>
           <li>Cart</li>
           <button
             className="login-btn"
